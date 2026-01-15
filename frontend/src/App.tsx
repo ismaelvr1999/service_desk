8a99d7b38625accce772409bfa5fcd04a02bf881
ismaelvr1999 <ismael.villalpando@alumnos.udg.mx>
2026-01-14 19:16:13 -0600

    frontend set up

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {  
+  return (
+    <div className='w-screen h-screen bg-primary'>
+      <h1 className='font-bold text-4xl text-body'>Hello World</h1>
+      <h1 className='font-bold text-4xl text-subtext-1'>Hello World</h1>
+      <h1 className='font-bold text-4xl text-subtext-0'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
